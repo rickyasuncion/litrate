08b760f8b79f294abcdd3f476e675eea892e972b
rickyasuncion <145637074+dootDoo@users.noreply.github.com>
2024-07-25 18:48:01 -0600

    add styling for search component

--- components/search.jsx
+++ components/search.jsx
@@ -14,22 +14,29 @@ const Search = ({ type, text }) => {
       switch (type) {
         case "Title":
           response = await fetch(
-            "https://www.googleapis.com/books/v1/volumes?q=" + text + "&maxResults=40"
+            "https://www.googleapis.com/books/v1/volumes?q=" +
+              text +
+              "&maxResults=40"
           );
           break;
         case "Author":
           response = await fetch(
-            "https://www.googleapis.com/books/v1/volumes?q=inauthor:" + text + "&maxResults=40"
+            "https://www.googleapis.com/books/v1/volumes?q=inauthor:" +
+              text +
+              "&maxResults=40"
           );
           break;
         case "ISBN":
           response = await fetch(
-            "https://www.googleapis.com/books/v1/volumes?q=isbn:" + text + "&maxResults=40"
+            "https://www.googleapis.com/books/v1/volumes?q=isbn:" +
+              text +
+              "&maxResults=40"
           );
           break;
       }
       const result = await response.json();
 
+      
       const volumes = await Promise.all(
         result.items.map(async (item) => {
           const res = await fetch(
@@ -45,25 +52,39 @@ const Search = ({ type, text }) => {
   }, [type, text]);
 
   return (
-    <div>
-      {books.map((book, index) => (
+    <div className="flex flex-col items-start p-5 mx-auto mt-10 w-1/2 bg-dark-blue border border-black rounded-lg shadow-lg">
+      {books.map((book, index) =>
         book.volumeInfo ? (
-          <div key={index}>
-            <div>
-              {book.volumeInfo.title}: {book.volumeInfo.authors?.join(", ")}
-            </div>
+          <div
+            className="flex p-8 mt-10 w-full gap-4 bg-cyan-800 rounded-md shadow-md"
+            key={index}
+          >
             {book.volumeInfo.imageLinks && (
               <Image
                 src={book.volumeInfo.imageLinks.thumbnail}
                 width={100}
                 height={100}
-                className="w-44 h-auto"
+                className="w-56 h-56"
                 alt="Picture of the book"
               />
             )}
+            <div className="ml-5 ">
+              <h2 className="text-xl font-bold text-white">
+                {book.volumeInfo.title}
+              </h2>
+              <p className="text-gray-300">
+                {book.volumeInfo.authors?.join(", ")}
+              </p>
+              <p className="">
+                {book.volumeInfo.description
+                  ?.substring(0, 350)
+                  .replace(/<[^>]*>?/gm, "")}
+                ...
+              </p>
+            </div>
           </div>
         ) : null
-      ))}
+      )}
     </div>
   );
 };
